19e445d6d83d603030574a0a849cfd14722f3bd3
Keshava Banda <71254056+keshavaBanda@users.noreply.github.com>
2024-12-15 17:16:41 +0530

    Fix server crash issue

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const { routeAuth, checkUser } = require('./middleware/authMiddleware');
 const app = express();
 
 // middleware
+const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cookieParser())
@@ -22,8 +23,14 @@ app.set('view engine', 'ejs');
 // database connection
 const dbURI = 'mongodb://localhost:27017/smothies-data';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-  .then((result) => app.listen(3000))
-  .catch((err) => console.log("ERROR ",err));
+  .then((result) => {
+    app.listen(PORT);
+    console.log("Server Running with ", PORT)
+  })
+  .catch((err) => {
+    console.log("ERROR ", err)
+    process.exit(1);
+  });
 
 // routes
 app.get('*', checkUser)
